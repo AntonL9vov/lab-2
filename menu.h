0a70d055c470ae1c61ec812f4c8dd04646b19b23
AntonLvov <lvov.anton.02@gmail.com>
2021-05-13 23:45:59 +0300

    second point

--- menu.h
+++ menu.h
@@ -29,11 +29,12 @@ int GetInt(int lower, int upper){
     cout<<"Input number: ";
     cin>>a;
     if(a>upper||a<lower){
-        cout<<"Number is out of range, try again";
+        cout<<"Number is out of range, try again"<<endl;
         GetInt(lower, upper);
     }
     return a;
 }
+
 int getNumber(int a){
     int b;
     cin>>b;
@@ -65,8 +66,8 @@ float randomFloat() {
     return static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/100));
 }
 
-complex<int> randomComplex() {
-    return complex<int>(randomFloat(), randomFloat());
+complex<float> randomComplex() {
+    return complex<float>(randomFloat(), randomFloat());
 }
 
 ostream& operator << (ostream& cout, complex<float> com){
@@ -212,8 +213,125 @@ void getVector(ArraySequence<Vector<int>*> *vectorInt, ArraySequence<Vector<floa
     cout<<"---------"<<endl;
 }
 
-void getMatrix(ArraySequence<Matrix<int>*> matrixInt, ArraySequence<Matrix<float>*> matrixFloat, ArraySequence<Matrix<complex<float>>*> matrixComplex;){
-cout<<"---------"<<endl;
+template<class T>
+void createMatrix(ArraySequence<Matrix<T>*> *array, int height, int length){
+    cout<<endl;
+    cout<<"Input 1,1 number:";
+    T a = getNumber(a);
+    T *r = new T[1];
+    r[0] = a;
+    ArraySequence<T> *arraySequence = new ArraySequence<T>(r, 1);
+    int j = 1;
+    for (int i = 0; i < height; ++i) {
+        for (j ; j < length; ++j) {
+            cout<<"Input "<<i+1<<", "<<j+1<<" number:";
+            arraySequence->Append(getNumber(a));
+        }
+        j = 0;
+    }
+    auto *matrix = new Matrix<T>(*arraySequence, length, height);
+    cout<<"it is your new matrix: "<<*matrix;
+    array->Append(matrix);
+}
+
+void createRandomIntMatrix(ArraySequence<Matrix<int>*> *array, int height, int length){
+    cout<<endl;
+    int a = randomInt();
+    int *r = new int[1];
+    r[0] = a;
+    ArraySequence<int> *arraySequence = new ArraySequence<int>(r, 1);
+    int j = 1;
+    for (int i = 0; i < height; ++i) {
+        for (j ; j < length; ++j) {
+            arraySequence->Append(randomInt());
+        }
+        j = 0;
+    }
+    auto *matrix = new Matrix<int>(*arraySequence, length, height);
+    cout<<"it is your new matrix: "<<*matrix;
+    array->Append(matrix);
+}
+
+void createRandomFloatMatrix(ArraySequence<Matrix<float>*> *array, int height, int length){
+    cout<<endl;
+    float a = randomFloat();
+    float *r = new float[1];
+    r[0] = a;
+    ArraySequence<float> *arraySequence = new ArraySequence<float>(r, 1);
+    int j = 1;
+    for (int i = 0; i < height; ++i) {
+        for (j ; j < length; ++j) {
+            arraySequence->Append(randomFloat());
+        }
+        j = 0;
+    }
+    auto *matrix = new Matrix<float>(*arraySequence, length, height);
+    cout<<"it is your new matrix: "<<*matrix;
+    array->Append(matrix);
+}
+
+void createRandomComplexMatrix(ArraySequence<Matrix<complex<float>>*> *array, int height, int length){
+    cout<<endl;
+    complex<float> a = randomComplex();
+    complex<float> *r = new complex<float>[1];
+    r[0] = a;
+    ArraySequence<complex<float>> *arraySequence = new ArraySequence<complex<float>>(r, 1);
+    int j = 1;
+    for (int i = 0; i < height; ++i) {
+        for (j ; j < length; ++j) {
+            arraySequence->Append(randomComplex());
+        }
+        j = 0;
+    }
+    auto *matrix = new Matrix<complex<float>>(*arraySequence, length, height);
+    cout<<"it is your new matrix: "<<*matrix;
+    array->Append(matrix);
+}
+
+void getMatrix(ArraySequence<Matrix<int>*> *matrixInt, ArraySequence<Matrix<float>*> *matrixFloat, ArraySequence<Matrix<complex<float>>*> *matrixComplex){
+    cout<<"---------"<<endl;
+    cout<<"What type of matrix do you want?"<<endl;
+    cout<<"1. Int."<<endl;
+    cout<<"2. Float."<<endl;
+    cout<<"3. Complex."<<endl;
+    int a = GetInt(1, 3);
+
+    cout<<"Input matrix length:";
+    int length = GetInt();
+    cout<<"Input matrix height: ";
+    int height = GetInt();
+
+    int ch;
+    cout<<endl;
+    cout<<"How do you want to input your matrix?"<<endl;
+    cout<<"1. Randomly."<<endl;
+    cout<<"2. By myself."<<endl;
+    ch = GetInt(1, 2);
+    switch (a) {
+        case 1:
+            if(ch==1) {
+                createRandomIntMatrix(matrixInt, height, length);
+            }else{
+                createMatrix(matrixInt, height, length);
+            }
+            break;
+        case 2:
+            if (ch==1){
+                createRandomFloatMatrix(matrixFloat, height, length);
+            }else{
+                createMatrix(matrixFloat, height, length);
+            }
+            break;
+        case 3:
+            if (ch==1){
+                createRandomComplexMatrix(matrixComplex, height, length);
+            }else{
+                createMatrix(matrixComplex, height, length);
+            }
+            break;
+        default: break;
+    }
+    cout<<"---------"<<endl;
 }
 
 void menu(){
@@ -247,7 +365,9 @@ void menu(){
         switch (number) {
             case 1: getVector(&vectorInt, &vectorFloat, &vectorComplex);
                 break;
-            case 2: break;
+            case 2:
+                getMatrix(&matrixInt, &matrixFloat, &matrixComplex);
+                break;
             case 3: break;
             case 4: break;
             case 5: break;
